4bf452dd97dd445e3e9ef843f004bec3d5527491
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2023-10-29 18:13:23 +0600

    dashboard-setup

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,10 @@
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
     <div className="home">
       <h3>this is home page</h3>
+      <Link to='/dashboard'>dashboard</Link>
     </div>
   );
 };

48d54da76f73336903fcf40fdb92b46c34445d1d
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2023-12-01 16:16:07 +0600

    update-qutation-page

--- src/pages/Home/AddJobCard/Qutation.jsx
+++ src/pages/Home/AddJobCard/Qutation.jsx
@@ -148,7 +148,7 @@ const Qutation = () => {
             <button className="">Add To Qutation </button>
           </div>
           <div className="buttonGroup mt-8">
-            <Link to="/dashboard/details">
+            <Link to="/details">
               <button>Preview</button>
             </Link>
             <button>Download </button>
@@ -158,23 +158,23 @@ const Qutation = () => {
         </form>
       </div>
       <div className="overflow-x-auto mt-20">
-      <div className="flex items-center justify-between mb-5">
-        <h3 className="text-3xl font-bold mb-3">Qutation List:</h3>
-        <div className="flex items-center searcList">
-          <select
-          onChange={(e)=>setSelect(e.target.value)}
-          >
-            <option value="SL No"> SL No</option>
-            <option value="Customer Name"> Customer Name</option>
-            <option value="Order Number"> Order Number</option>
-            <option value="Car Number"> Car Number</option>
-            <option value="Mobile Number"> Mobile Number</option>
-          </select>
-        <div className="searchGroup">
-          <input autoComplete="off" type="text" placeholder={select} />
-        </div>
-        <button className="SearchBtn ">Search </button>
-        </div>
+        <div className="flex items-center justify-between mb-5">
+          <h3 className="text-3xl font-bold mb-3">Qutation List:</h3>
+          <div className="flex items-center searcList">
+            <select
+              onChange={(e) => setSelect(e.target.value)}
+            >
+              <option value="SL No"> SL No</option>
+              <option value="Customer Name"> Customer Name</option>
+              <option value="Order Number"> Order Number</option>
+              <option value="Car Number"> Car Number</option>
+              <option value="Mobile Number"> Mobile Number</option>
+            </select>
+            <div className="searchGroup">
+              <input autoComplete="off" type="text" placeholder={select} />
+            </div>
+            <button className="SearchBtn ">Search </button>
+          </div>
         </div>
         <table className="table ">
           <thead className="tableWrap">
@@ -198,14 +198,14 @@ const Qutation = () => {
               <td>10-05-2023</td>
               <td>
                 <div className="editIconWrap">
-                  <Link to="/dashboard/details">
+                  <Link to="/details">
                     <FaEye className="editIcon" />
                   </Link>
                 </div>
               </td>
               <td>
                 <div className="editIconWrap">
-                  <Link to="/dashboard/update-customer">
+                  <Link to="/update-customer">
                     <FaEdit className="editIcon" />
                   </Link>
                 </div>
@@ -225,14 +225,14 @@ const Qutation = () => {
               <td>10-05-2023</td>
               <td>
                 <div className="editIconWrap">
-                  <Link to="/dashboard/details">
+                  <Link to="/details">
                     <FaEye className="editIcon" />
                   </Link>
                 </div>
               </td>
               <td>
                 <div className="editIconWrap">
-                  <Link to="/dashboard/update-customer">
+                  <Link to="/update-customer">
                     <FaEdit className="editIcon" />
                   </Link>
                 </div>
@@ -252,14 +252,14 @@ const Qutation = () => {
               <td>10-05-2023</td>
               <td>
                 <div className="editIconWrap">
-                  <Link to="/dashboard/details">
+                  <Link to="/details">
                     <FaEye className="editIcon" />
                   </Link>
                 </div>
               </td>
               <td>
                 <div className="editIconWrap">
-                  <Link to="/dashboard/update-customer">
+                  <Link to="/update-customer">
                     <FaEdit className="editIcon" />
                   </Link>
                 </div>
@@ -279,14 +279,14 @@ const Qutation = () => {
               <td>10-05-2023</td>
               <td>
                 <div className="editIconWrap">
-                  <Link to="/dashboard/details">
+                  <Link to="/details">
                     <FaEye className="editIcon" />
                   </Link>
                 </div>
               </td>
               <td>
                 <div className="editIconWrap">
-                  <Link to="/dashboard/update-customer">
+                  <Link to="/update-customer">
                     <FaEdit className="editIcon" />
                   </Link>
                 </div>
@@ -306,14 +306,14 @@ const Qutation = () => {
               <td>10-05-2023</td>
               <td>
                 <div className="editIconWrap">
-                  <Link to="/dashboard/details">
+                  <Link to="/details">
                     <FaEye className="editIcon" />
                   </Link>
                 </div>
               </td>
               <td>
                 <div className="editIconWrap">
-                  <Link to="/dashboard/update-customer">
+                  <Link to="/update-customer">
                     <FaEdit className="editIcon" />
                   </Link>
                 </div>

08cd2e10dc2695b59fb253d23005c41426610ff2
Ibrahim-Sikder <ibrahimsikder5033@gmail.com>
2023-12-19 17:05:41 +0600

    modify-login-page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,64 +1,56 @@
 /* eslint-disable no-unused-vars */
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './Login.css'
 import Swal from "sweetalert2";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
+
 const Login = () => {
-  const [shwoPassword, setShowPassword] = useState(null)
-  const navigate = useNavigate()
-  const email = useRef()
-  const password = useRef()
-  const handleShowPassword = ()=>{
-    setShowPassword((shwoPassword)=>!shwoPassword)
-  }
-  const handleLogin = event => {
-    event.preventDefault()
-    console.log(email.current.value, password.current.value)
-    if (email.current.value == 'trust@gmail.com' && password.current.value == 'trust123') {
+  const [showPassword, setShowPassword] = useState(false);
+  const [defaultEmail, setDefaultEmail] = useState('');
+  const [defaultPassword, setDefaultPassword] = useState('');
+  const navigate = useNavigate();
+  const email = useRef();
+  const password = useRef();
+
+  const handleShowPassword = () => {
+    setShowPassword((showPassword) => !showPassword);
+  };
+
+  const handleLogin = (event) => {
+    event.preventDefault();
+    console.log(email.current.value, password.current.value);
+    if (email.current.value === 'trust@gmail.com' && password.current.value === 'trust123') {
       Swal.fire({
         position: 'center',
         icon: 'success',
-        title: 'Login Successfully ! ',
+        title: 'Login Successfully!',
         showConfirmButton: false,
         timer: 1500
-      })
-      navigate('/dashboard')
+      });
+      navigate('/dashboard');
     } else {
       Swal.fire({
         icon: "error",
         position: 'center',
- 
-      
         title: 'Invalid Email & Password',
         showConfirmButton: false,
         timer: 1500
-      })
- 
-        
-     }
-  }
-  const ref = useRef(null);
-  // useEffect(() => {
-  //   import("@lottiefiles/lottie-player");
-  // });
+      });
+    }
+  };
+
+  const handleRowClick = (emailValue, passwordValue) => {
+    setDefaultEmail(emailValue);
+    setDefaultPassword(passwordValue);
+  };
+
   return (
     <div>
-      <div className="py-5  bg-[#351E98] min-h-screen flex items-center justify-center ">
-        <div className="animationWrap">
-        <lottie-player
-                    id="firstLottie"
-                    ref={ref}
-                    autoplay
-                    loop
-                    mode="normal"
-                    src="/car5.json"
-                    className="animation"
-                  ></lottie-player>
-        </div>
+      <div className="py-5 bg-[#351E98] min-h-screen flex items-center justify-center">
         <div>
           <h2 className="text-4xl capitalize mb-8 font-bold text-white text-center">
-            Trust Auto Solution !
+            Login Now!
           </h2>
           <div className="signUnWrap">
             <form onSubmit={handleLogin}>
@@ -69,6 +61,7 @@ const Login = () => {
                     ref={email}
                     name="email"
                     placeholder="Email"
+                    value={defaultEmail}
                     type="email"
                     className="inputFiel"
                     autoComplete="off"
@@ -79,14 +72,15 @@ const Login = () => {
                   <input
                     ref={password}
                     name="password"
+                    value={defaultPassword}
+                    type={showPassword}
                     placeholder="Password"
-                    type={shwoPassword ? 'text' : 'password'}
                     className="inputFiel"
                     autoComplete="off"
                   />
-               <div onClick={handleShowPassword} className="showIconWrap">
-             {shwoPassword ?   <FaEye className="showPassIcon"/> : <FaEyeSlash className="showPassIcon" />}
-               </div>
+                  <div onClick={handleShowPassword} className="showIconWrap">
+                    {showPassword ? <FaEye className="showPassIcon" /> : <FaEyeSlash className="showPassIcon" />}
+                  </div>
                 </div>
                 <div className="singleSignupForm">
                   <button type="submit">
@@ -94,19 +88,23 @@ const Login = () => {
                   </button>
                 </div>
               </div>
-
-
-
-              {/* <div className="my-8 signupLine"> </div> */}
-              {/* <div className="text-center">
-                <p>
-                  Don’t have an account an account?
-                  <Link to="/">
-                    <b className="text-[#351E98]"> Sign Up </b>
-                  </Link>
-                </p>
-              </div> */}
             </form>
+            <table className="loginTable mt-3">
+              <thead>
+                <tr>
+                  <th>Email</th>
+                  <th>Password</th>
+                  <th>Role</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr onClick={() => handleRowClick('trust@gmail.com', 'trust123')}>
+                  <td><span>trust@gmail.com</span></td>
+                  <td><span>trust123</span></td>
+                  <td><span>Admin</span></td>
+                </tr>
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
